b9c20f1d2a2164a186d0703a3d25e03768e41894
lenakasalitsa10 <130748311+lenakasalitsa10@users.noreply.github.com>
2023-12-05 09:35:25 +0300

    Update Problem.cs

--- Program5/Zadanie5/Problem.cs
+++ Program5/Zadanie5/Problem.cs
@@ -1,8 +1,8 @@
 namespace Zadanie5;
 
-public class Problem : Exception    //конструктор  //объявление класса Problem, который наследуется от класса Exception
+public class Problem : Exception    //объявление класса Problem, который наследуется от класса Exception
 {
-    public Problem() : base("Время не может равняться 0")
+    public Problem() : base("Время не может равняться 0")   //конструктор
     {
     }
 }
